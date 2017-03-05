a3cd6ea1509d869215722b252c754acaa953fab4
Michael Martorella <michaelmartorella@gmail.com>
2017-03-05 15:56:19 -0500

    Remove video filepath on homepage

--- honestenergy/settings.py
+++ honestenergy/settings.py
@@ -22,7 +22,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False if os.environ.get('VENV') == 'production' else True
+DEBUG = False# if os.environ.get('VENV') == 'production' else True
 
 # Application definition
 
@@ -112,30 +112,30 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-if not DEBUG:
-    # SECURITY WARNING: keep the secret key used in production secret!
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-
-    # Update database configuration with $DATABASE_URL.
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)
-
-    GOOGLE_ANALYTICS_ID = os.environ.get('GOOGLE_ANALYTICS_ID')
-
-    MAILCHIMP_USERNAME = os.environ.get('MAILCHIMP_USERNAME')
-    MAILCHIMP_SECRET_KEY = os.environ.get('MAILCHIMP_SECRET_KEY')
-    MAILCHIMP_LISTS = {
-        'Leads': 'c3fa5fc97b',
-    }
-
-else:
-    SECRET_KEY = 'XXXXXXXXXXXXXX'
-    GOOGLE_ANALYTICS_ID = 'UA-XXXXX-X'
-    MAILCHIMP_USERNAME = 'XXXXXXXXX'
-    MAILCHIMP_SECRET_KEY = 'XXXXXXXXX'
-    MAILCHIMP_LISTS = {
-        'Leads': 'XXXXXXXXX',
-    }
+# if not DEBUG:
+#     # SECURITY WARNING: keep the secret key used in production secret!
+#     SECRET_KEY = os.environ.get('SECRET_KEY')
+#
+#     # Update database configuration with $DATABASE_URL.
+#     db_from_env = dj_database_url.config(conn_max_age=500)
+#     DATABASES['default'].update(db_from_env)
+#
+#     GOOGLE_ANALYTICS_ID = os.environ.get('GOOGLE_ANALYTICS_ID')
+#
+#     MAILCHIMP_USERNAME = os.environ.get('MAILCHIMP_USERNAME')
+#     MAILCHIMP_SECRET_KEY = os.environ.get('MAILCHIMP_SECRET_KEY')
+#     MAILCHIMP_LISTS = {
+#         'Leads': 'c3fa5fc97b',
+#     }
+#
+# else:
+#     SECRET_KEY = 'XXXXXXXXXXXXXX'
+#     GOOGLE_ANALYTICS_ID = 'UA-XXXXX-X'
+#     MAILCHIMP_USERNAME = 'XXXXXXXXX'
+#     MAILCHIMP_SECRET_KEY = 'XXXXXXXXX'
+#     MAILCHIMP_LISTS = {
+#         'Leads': 'XXXXXXXXX',
+#     }
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -164,53 +164,53 @@ FIXTURE_DIRS = [
     ]
 
 
-# # Debug code to debug production server errors locally
-# # As seen here: http://stackoverflow.com/questions/238081/how-do-you-log-server-errors-on-django-sites/6395837#6395837
-# # DEBUG must be set to false
-# DEBUG_LOGFILE = '/var/log/django/honestenergy.log'
-# SECRET_KEY = 'XXXXXXXXXXXXXX'
-# GOOGLE_ANALYTICS_ID = 'UA-XXXXX-X'
-# MAILCHIMP_USERNAME = 'XXXXXXXXX'
-# MAILCHIMP_SECRET_KEY = 'XXXXXXXXX'
-# MAILCHIMP_LISTS = {
-#     'Leads': 'XXXXXXXXX',
-# }
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'handlers': {
-#         # Include the default Django email handler for errors
-#         # This is what you'd get without configuring logging at all.
-#         'mail_admins': {
-#             'class': 'django.utils.log.AdminEmailHandler',
-#             'level': 'ERROR',
-#              # But the emails are plain text by default - HTML is nicer
-#             'include_html': True,
-#         },
-#         # Log to a text file that can be rotated by logrotate
-#         'logfile': {
-#             'class': 'logging.handlers.WatchedFileHandler',
-#             'filename': DEBUG_LOGFILE
-#         },
-#     },
-#     'loggers': {
-#         # Again, default Django configuration to email unhandled exceptions
-#         'django.request': {
-#             'handlers': ['mail_admins'],
-#             'level': 'ERROR',
-#             'propagate': True,
-#         },
-#         # Might as well log any errors anywhere else in Django
-#         'django': {
-#             'handlers': ['logfile'],
-#             'level': 'ERROR',
-#             'propagate': False,
-#         },
-#         # Your own app - this assumes all your logger names start with "honestenergy."
-#         'honestenergy': {
-#             'handlers': ['logfile'],
-#             'level': 'WARNING', # Or maybe INFO or DEBUG
-#             'propagate': False
-#         },
-#     },
-# }
+# Debug code to debug production server errors locally
+# As seen here: http://stackoverflow.com/questions/238081/how-do-you-log-server-errors-on-django-sites/6395837#6395837
+# DEBUG must be set to false
+DEBUG_LOGFILE = '/var/log/django/honestenergy.log'
+SECRET_KEY = 'XXXXXXXXXXXXXX'
+GOOGLE_ANALYTICS_ID = 'UA-XXXXX-X'
+MAILCHIMP_USERNAME = 'XXXXXXXXX'
+MAILCHIMP_SECRET_KEY = 'XXXXXXXXX'
+MAILCHIMP_LISTS = {
+    'Leads': 'XXXXXXXXX',
+}
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        # Include the default Django email handler for errors
+        # This is what you'd get without configuring logging at all.
+        'mail_admins': {
+            'class': 'django.utils.log.AdminEmailHandler',
+            'level': 'ERROR',
+             # But the emails are plain text by default - HTML is nicer
+            'include_html': True,
+        },
+        # Log to a text file that can be rotated by logrotate
+        'logfile': {
+            'class': 'logging.handlers.WatchedFileHandler',
+            'filename': DEBUG_LOGFILE
+        },
+    },
+    'loggers': {
+        # Again, default Django configuration to email unhandled exceptions
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        # Might as well log any errors anywhere else in Django
+        'django': {
+            'handlers': ['logfile'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        # Your own app - this assumes all your logger names start with "honestenergy."
+        'honestenergy': {
+            'handlers': ['logfile'],
+            'level': 'WARNING', # Or maybe INFO or DEBUG
+            'propagate': False
+        },
+    },
+}
